7e7773d75a2ae63ac9cddd1494c5a40d60fd082c
Down Ramp Apps <98784052+DownRamp@users.noreply.github.com>
2022-05-31 20:42:14 -0400

    update

--- investing.py
+++ investing.py
@@ -1,7 +1,29 @@
 
+def save():
+    print()
 
+def reader():
+    print()
 
-# dividend
+# Add up dividends for month and save
+def dividend():
 
-# growth
+# Add up months growth and save
+# last month give final check
+def growth():
+    print()
+
 # time until millionaire estimate
+def compound():
+    # return years until millionaire if no addition
+
+    print()
+
+def window():
+    # show dividend changes
+    # show growth changes
+    # show breakdown of dividends
+    # show breakdown of growth
+    # show list of all stocks
+    print()
+
